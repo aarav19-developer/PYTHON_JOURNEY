95fe7bbc143a4012eca67e9cce054317045f32ed
AARAV_19 <aaravharit418@gmail.com>
2026-01-18 23:01:03 +0530

    First Stage

---
+++ 1_variables.py
@@ -0,0 +1,42 @@
+''' 
+VARIABLES:- 
+
+A container for a value ( string, integer, float, boolean).
+A variables behave as if it was the value i containes.
+
+''' 
+# String: 
+   
+first_name = "DEAR ZINDAGI"
+email =  "dearzindagi19@gmail.com"
+
+print(f"Hello {first_name}")
+
+# Integer:
+
+age = 19
+number_of_student = 22
+
+print(f"I love {age}")
+print(f"There are total {number_of_student} students in my batch")
+
+
+# Float:
+
+price = 10.99
+gpa = 10.10
+
+print(f"The price is ${price}")
+print(f"my GPA is {gpa}")
+
+# Boolean:
+# ONLY contains TRUE and FALSE.
+
+is_online = input("Enter you answer IN TRUE OR FALSE: ")
+print(f"you are online: {is_online}")
+
+
+
+
+
+
