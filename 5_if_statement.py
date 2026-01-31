1f0ce3705da3fc9428d32b86f5465288efbf4a6b
Aarav Harit <aaravharit418@gmail.com>
2026-01-31 15:58:15 +0530

    Fifth Stage

---
+++ 5_if_statement.py
@@ -0,0 +1,9 @@
+# if = Do some code only IF some condition is True.
+# Else do something else.
+
+age = int(input("enter your age: "))
+
+if age >= 18:
+    print("you can vote")
+else:
+    print("you can not vote")  
