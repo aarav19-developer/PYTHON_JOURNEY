618596d068584feb1d0e02081b645e13f4b095dd
Aarav Harit <aaravharit418@gmail.com>
2026-01-31 15:46:56 +0530

    Fifteen Stage

---
+++ 15_countdown_timer.py
@@ -0,0 +1,23 @@
+# import time
+
+# my_time = int(input("enter the time in seconds: "))
+
+# for x in range(0, my_time):
+#     print(x)
+#     time.sleep(1)
+
+
+# print("Times UP!")
+
+
+# my_time = int(input("enter the time in seconds: "))
+
+# for x in range(my_time, 0, -1):
+# for x in range(0, my_time):
+#     seconds = x % 60
+#     minutes = int(x/60) % 60
+#     hours = int(x / 3600)
+#     print(f"{hours: 02}: {minutes:02}:{seconds:02}")
+#     time.sleep(1)
+
+# print("Time's UP!")
