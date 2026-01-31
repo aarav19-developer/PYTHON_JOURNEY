177d356ff6013b1a82befb01102221ab7510ddc6
Aarav Harit <aaravharit418@gmail.com>
2026-01-31 22:40:18 +0530

    JOURNEY_PART

---
+++ 17_list_set_tupple.py
@@ -0,0 +1,21 @@
+# COLLECTION:- single "varibale" used to store multiple values.
+
+# LIST = [] ordered and changeable. Duplicate OK.
+# METHOD:
+# 1. .append()
+# 2. .remove()
+# 3. .reverse()
+# 4. .clear()
+# 5. .sort()
+# 6. .index()
+# 7. .count()
+# 8. .pop()
+
+# SET = {} unordered and immutable, but Add/Remove OK. NO duplicates.
+# METHOD:
+# 1. .add()
+# 2. .pop()
+# 3. .remove()
+
+# TUPLE = () ordered ad unchangeable. Duplicates OK. Fatser than list.
+
