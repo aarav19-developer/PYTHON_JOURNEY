1e8d6eea17ebb9fb3e293fc84a137bed04d36ab4
Aarav Harit <aaravharit418@gmail.com>
2026-03-18 16:21:55 +0530

    JOURNEY_PART

---
+++ 39_encryption_program.py
@@ -0,0 +1,38 @@
+import random
+import string
+
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+print(f"Chars:   {chars}")
+print(f"Key:     {key}")
+
+# ENCRYPT:-
+
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = " "
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"original messsage: {plain_text}")
+print(f"encrypted messsage: {cipher_text}")
+
+
+# DECRYPT:-
+
+cipher_text = input("Enter a message to encrypt: ")
+plain_text = " "
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted messsage: {cipher_text}")
+print(f"original messsage:  {plain_text}")
